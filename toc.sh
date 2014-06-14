81271cba7cc8843721e2b479ed2c65ea987e35ef
Matt Butcher <technosophos@gmail.com>
2014-06-13 23:35:16 -0600

    Testing out a table of contents.

---
+++ toc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DEST="table_of_contents.md"
+BASE="https://github.com/Masterminds/cookoo-web-tutorial/tree"
+
+echo "# Contents" > $DEST
+
+list=`git branch --list '*_*'`
+for i in $list; do
+  echo $i
+  echo "1. [$i]($BASE/$i)" >> $DEST
+done
